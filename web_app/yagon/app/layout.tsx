3cdfac62b867f6912a10350b72068c5bea2112d6
Rémi Champlon <remi.champlon@vold.lu>
2023-11-27 19:32:59 +0100

    add web3.js

--- web_app/yagon/app/layout.tsx
+++ web_app/yagon/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Yagon',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' bg-main'}>{children}</body>
+      <body className={'font-sans'}>{children}</body>
     </html>
   )
 }
--- web_app/yagon/app/page.tsx
+++ web_app/yagon/app/page.tsx
@@ -1,14 +1,62 @@
+"use client";
 import Link from "next/link";
 import QrCode from "@/app/icons/qr-code";
-import Search from "@/app/icons/search";
+import {useEffect, useState} from "react";
+import Web3 from 'web3';
 
 export default function Home() {
+
+    const [web3, setWeb3] = useState(null);
+    const [account, setAccount] = useState(null);
+
+    useEffect(() => {
+        if (!web3) {
+            initWeb3().then(r => console.log('web3 connected'))
+        }
+    }, [web3]);
+
+
+    useEffect(() => {
+        fetchAccount().then(r => console.log('accound fetched'));
+    }, [web3]);
+
+
+    const initWeb3 = async () => {
+        // @ts-ignore
+        if (window.ethereum) {
+            // @ts-ignore
+            const newWeb3 = new Web3(window.ethereum);
+            try {
+                // @ts-ignore
+                await window.ethereum.request({method: 'eth_requestAccounts'});
+                // @ts-ignore
+                setWeb3(newWeb3);
+            } catch (error) {
+                console.error('User denied account access');
+            }
+        } else if (window.web3) {
+            // @ts-ignore
+            setWeb3(new Web3(window.web3.currentProvider));
+        } else {
+            console.log('No Ethereum browser extension detected');
+        }
+    }
+
+    const fetchAccount = async () => {
+        if (web3) {
+            // @ts-ignore
+            const accounts = await web3.eth.getAccounts();
+            setAccount(accounts[0]);
+        }
+    };
+
     return (
-        <main className="flex min-h-screen flex-col items-center">
+        <main className="bg-main flex min-h-screen flex-col items-center">
+            <p className={'absolute right-0 p-1 text-gray-600'}>{account ? 'Wallet connected: ' + account : 'No wallet connected'}</p>
             <p className={"text-primary text-xl mt-2"}>Yagon</p>
             <div className={'mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32'}>
                 <div className="flex flex-col gap-8 text-center mx-auto max-w-4xl">
-                    <h1 className={'text-6xl font-medium leading-snug'}>The new way to do the <span
+                    <h1 className={'text-5xl md:text-6xl font-medium leading-snug'}>The new way to do the <span
                         className={'text-primary'}>supply chain tracking</span></h1>
                 </div>
             </div>
@@ -17,11 +65,7 @@ export default function Home() {
                     <input type="text" name="product_id" id="product_id"
                            className="w-full text-2xl font-light rounded-full border border-gray-300 bg-white shadow-lg px-4 text-center py-2"
                            placeholder="Find the product"/>
-                    <button
-                        className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
-                        <Search/>
-                    </button>
-                    <p className={'text-3xl text-gray-300 md:mx-3'}>|</p>
+
                     <button
                         className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
                         <QrCode/>
