73965cd44c6b4a7812dba308d5032b05dcf43023
Rémi Champlon <remi.champlon@vold.lu>
2023-11-27 17:57:32 +0100

    fix

--- web_app/yagon/app/page.tsx
+++ web_app/yagon/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import QrCode from "@/app/icons/qr-code";
+import Search from "@/app/icons/search";
 
 export default function Home() {
     return (
@@ -11,9 +13,18 @@ export default function Home() {
                 </div>
             </div>
             <div className={'flex flex-col max-w-xl w-full items-center gap-6'}>
-                <input type="text" name="product_id" id="product_id"
-                       className="w-full text-2xl font-light rounded-full border border-gray-300 bg-white shadow-lg px-4 text-center py-2"
-                       placeholder="Find the product" />
+                <div className={'flex flex-row gap-2 w-full items-center'}>
+                    <input type="text" name="product_id" id="product_id"
+                           className="w-full text-2xl font-light rounded-full border border-gray-300 bg-white shadow-lg px-4 text-center py-2"
+                           placeholder="Find the product" />
+                    <button className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
+                        <Search />
+                    </button>
+                    <p className={'text-3xl text-gray-300 mx-3'}>|</p>
+                    <button className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
+                        <QrCode />
+                    </button>
+                </div>
                 <p className={'text-3xl font-medium'}>OR</p>
                 <Link href={''} className={" text-2xl font-light rounded-full bg-primary text-white shadow-lg px-8 text-center py-2 hover:bg-primary hover:bg-opacity-90 transition duration-200"}>
                     Create new one
