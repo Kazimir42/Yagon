789a2f3487e5477c71a405138a739b724dec2e5a
Rémi Champlon <remi.champlon@vold.lu>
2023-11-27 18:02:33 +0100

    design

--- web_app/yagon/app/page.tsx
+++ web_app/yagon/app/page.tsx
@@ -12,24 +12,27 @@ export default function Home() {
                         className={'text-primary'}>supply chain tracking</span></h1>
                 </div>
             </div>
-            <div className={'flex flex-col max-w-xl w-full items-center gap-6'}>
+            <div className={'flex flex-col max-w-xl w-full items-center gap-6 p-4'}>
                 <div className={'flex flex-row gap-2 w-full items-center'}>
                     <input type="text" name="product_id" id="product_id"
                            className="w-full text-2xl font-light rounded-full border border-gray-300 bg-white shadow-lg px-4 text-center py-2"
-                           placeholder="Find the product" />
-                    <button className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
-                        <Search />
+                           placeholder="Find the product"/>
+                    <button
+                        className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
+                        <Search/>
                     </button>
-                    <p className={'text-3xl text-gray-300 mx-3'}>|</p>
-                    <button className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
-                        <QrCode />
+                    <p className={'text-3xl text-gray-300 md:mx-3'}>|</p>
+                    <button
+                        className={'text-2xl font-light rounded-full bg-primary text-white shadow-lg px-3 py-3 text-center hover:bg-primary hover:bg-opacity-90 transition duration-200'}>
+                        <QrCode/>
                     </button>
                 </div>
                 <p className={'text-3xl font-medium'}>OR</p>
-                <Link href={''} className={" text-2xl font-light rounded-full bg-primary text-white shadow-lg px-8 text-center py-2 hover:bg-primary hover:bg-opacity-90 transition duration-200"}>
+                <Link href={''}
+                      className={" text-2xl font-light rounded-full bg-primary text-white shadow-lg px-8 text-center py-2 hover:bg-primary hover:bg-opacity-90 transition duration-200"}>
                     Create new one
                 </Link>
             </div>
         </main>
-)
+    )
 }
